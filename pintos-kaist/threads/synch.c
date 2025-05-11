4a56b2031b883f87df4db20b6aeb2bca62eb6af0
joho54 <joho0504@gmail.com>
2025-05-12 01:57:38 +0900

    [TEST] 세마포 테스팅 중

--- pintos-kaist/threads/synch.c
+++ pintos-kaist/threads/synch.c
@@ -66,7 +66,8 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		list_push_back (&sema->waiters, &thread_current ()->elem);
+		// list_push_back (&sema->waiters, &thread_current ()->elem);
+		list_insert_ordered(&sema->waiters, &thread_current ()->elem, cmp_priority, NULL);
 		thread_block ();
 	}
 	sema->value--;
@@ -112,6 +113,7 @@ sema_up (struct semaphore *sema) {
 	if (!list_empty (&sema->waiters))
 		thread_unblock (list_entry (list_pop_front (&sema->waiters),
 					struct thread, elem));
+		list_sort(&sema->waiters, cmp_priority, NULL);
 	sema->value++;
 	intr_set_level (old_level);
 }
