39da78a73b8311fc43afbfc5ad6ce9250c05891c
joho54 <joho0504@gmail.com>
2025-05-12 02:16:58 +0900

    [FEAT] sema, cond 최소 사양 구현 - priority-sema 가장 근접하게 결과 나오지만 수정 필요

--- pintos-kaist/threads/synch.c
+++ pintos-kaist/threads/synch.c
@@ -66,7 +66,6 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		// list_push_back (&sema->waiters, &thread_current ()->elem);
 		list_insert_ordered(&sema->waiters, &thread_current ()->elem, cmp_priority, NULL);
 		thread_block ();
 	}
@@ -284,7 +283,8 @@ cond_wait (struct condition *cond, struct lock *lock) {
 	ASSERT (lock_held_by_current_thread (lock));
 
 	sema_init (&waiter.semaphore, 0);
-	list_push_back (&cond->waiters, &waiter.elem);
+	// list_push_back (&cond->waiters, &waiter.elem);
+	list_insert_ordered(&cond->waiters, &waiter.elem, cmp_priority, NULL);
 	lock_release (lock);
 	sema_down (&waiter.semaphore);
 	lock_acquire (lock);
@@ -307,6 +307,7 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED) {
 	if (!list_empty (&cond->waiters))
 		sema_up (&list_entry (list_pop_front (&cond->waiters),
 					struct semaphore_elem, elem)->semaphore);
+		list_sort(&cond->waiters, cmp_priority, NULL);
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
